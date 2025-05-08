ccdb59f8afc153c4359bdf4093ab860a2b933753
Trinh Thi Kim Oanh <124591460+kimoanh0202@users.noreply.github.com>
2025-05-08 17:29:32 +0700

    update

--- src/main/java/Common/Constant.java
+++ src/main/java/Common/Constant.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class Constant {
     public static WebDriver WEBDRIVER;
-    public static final String Bookese_Admin = "https://bookese-admin.vercel.app/login";
+    public static final String Bookese_Admin = "";
     public static final String username = "admin@gmail.com";
     public static final String password = "admin1234@";
 }
